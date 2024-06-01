59a50c1c3ae59e9ce4d8093deacceea08ac86c42
ribeiro-henrique <hesr.ribeiro@gmail.com>
2024-06-01 10:32:38 -0300

    adiciona rota de produtos

--- src/Produtos/produtos.controller.ts
+++ src/Produtos/produtos.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from "@nestjs/common";
 import { ProdutosService } from "./produtos.service";
 
-@Controller()
+@Controller('produtos')
 export class ProdutosController {
   constructor(private readonly produtosService: ProdutosService) {}
   @Get()
